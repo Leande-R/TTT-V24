91e229fd25d41e3ea7b5250a5ccc8309fd9bd567
Leande-R <131276205+Leande-R@users.noreply.github.com>
2024-06-12 15:37:29 +0200

    add Basic Class Structure

---
+++ TicTacToe/src/main/java/ttt/Player.java
@@ -0,0 +1,20 @@
+package ttt;
+
+public class Player {
+
+    private char marker;
+
+
+    public char getMarker() {
+        return marker;
+    }
+
+    public void setMarker(char marker) {
+        this.marker = marker;
+
+
+    }
+
+
+
+}
