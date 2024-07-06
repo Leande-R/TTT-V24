d2e893c7de80680a64e850517c8557897082e928
wi23b186 <wi23b186@technikum-wien.at>
2024-07-06 19:22:23 +0200

    Implement TicTacToe move functionality for User Story 1

--- TicTacToe/src/main/java/ttt/TicTacToe.java
+++ TicTacToe/src/main/java/ttt/TicTacToe.java
@@ -1,38 +1,58 @@
 package ttt;
-
+import java.util.Scanner;
 public class TicTacToe {
 
     private Player player1;
-
     private Player player2;
-
-    private Player curentPlayer;
-
-
+    private Player currentPlayer;
     private Board board;
 
 
-    public TicTacToe(Player player1, Player player2, Player curentPlayer, Board board) {
-        this.player1 = player1;
-        this.player2 = player2;
-        this.curentPlayer = curentPlayer;
-        this.board = board;
+    public TicTacToe() {
+        player1 = new Player('X');
+        player2 = new Player('O');
+        currentPlayer = player1;
+        board = new Board();
     }
-
 
     public TicTacToe (TicTacToe toe){
         this.player1 = toe.player1;
         this.player2 = toe.player2;
-        this.curentPlayer = toe.curentPlayer;
+        this.currentPlayer = toe.currentPlayer;
         this.board = toe.board;
     }
 
+    public void start() {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            board.print();
+            System.out.println("Current Player: " + currentPlayer.getMarker());
 
-    public void start(){}
+            int row, col;
+            while (true) {
+                System.out.print("row (0-2): ");
+                row = scanner.nextInt();
+                System.out.print("column (0-2): ");
+                col = scanner.nextInt();
+                if (board.isCellEmpty(row, col)) {
+                    break;
+                } else {
+                    System.out.println("Cell is not empty, try again.");
+                }
+            }
 
+            board.place(row, col, currentPlayer.getMarker());
+            switchCurrentPlayer();
+        }
+    }
 
-    private void switchCurrentPlayer(){}
-
+    public void switchCurrentPlayer() {
+        if (currentPlayer == player1) {
+            currentPlayer = player2;
+        } else {
+            currentPlayer = player1;
+        }
+    }
 
     private boolean hasWinner(){
         return false;
